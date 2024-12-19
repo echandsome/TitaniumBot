d1965d79d627d4f6c1c6e5d5b3b88bb35e6d9fcb
echandsome <echandsome@hotmail.com>
2024-12-19 09:38:22 -0800

    Signed-off-by: echandsome <echandsome@hotmail.com>

---
+++ app/errors/not_found_error.js
@@ -0,0 +1,16 @@
+const { CustomValidationError } = require('./custom_validation_error');
+
+class NotFoundError extends CustomValidationError {
+  constructor(message, request_user = null, request_params = null, request_body = null) {
+    super(message, 404, 'not_found', request_user, request_params, request_body);
+
+    this.name = 'NotFoundError';
+    this.message = message;
+    this.status = 404;
+    this.code = 'not_found';
+  }
+}
+
+module.exports = {
+  NotFoundError,
+};
